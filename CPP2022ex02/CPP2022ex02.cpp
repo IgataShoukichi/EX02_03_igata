2335acd2a66b3f41afff8308e37892488826ed32
igatashoukichi@gmail.com <igatashoukichi@gmail.com>
2022-10-06 12:51:47 +0900

    内容変更

--- CPP2022ex02/CPP2022ex02.cpp
+++ CPP2022ex02/CPP2022ex02.cpp
@@ -1,19 +1,15 @@
-﻿// CPP2022ex02.cpp : このファイルには 'main' 関数が含まれています。プログラム実行の開始と終了がそこで行われます。
-//
-
-#include <iostream>
+﻿#include <iostream>
 #include <algorithm>
 using namespace std;
 
 int main() {
 	const int DATA_NUM = 10;	// 配列の要素数
-
 	// 10人の学生のテストの得点を格納した配列
 	int point[] = { 85, 72, 63, 45, 100, 98, 52, 88, 74, 65 };
 	int i, j;			// 配列の要素番号（ループカウンタ）
 	char grade;			// 成績の評価
 
-	for (i = 0; i < DATA_NUM - 1; i++) {
+	for (i = 0; i < DATA_NUM; i++) {
 		for (j = i + 1; j < DATA_NUM; j++) {
 			if (point[i] < point[j]) {
 				int w = point[i];
@@ -21,15 +17,19 @@ int main() {
 				point[j] = w;
 			}
 		}
-		if (point[i] < 60) {
-			break;
+		if (point[i] >= 83) {
+			grade = 'A';
+		}
+		else if (point[i] >= 66) {
+			grade = 'B';
+		}
+		else if (point[i] > 50) {
+			grade = 'C';
 		}
 		else {
-			if (point[i] >= 80)grade = 'A';
-			else if (point[i] >= 70)grade = 'B';
-			else grade = 'C';
-			cout << point[i] << "=" << grade << endl;
+			grade = 'D';
 		}
+		cout << point[i] << " = " << grade << endl;
 	}
 	return 0;
 }
